1882f146dbfee976c3aaac52897263462bec3d3e
Elias Vasquez <evasq710@hotmail.com>
2021-09-11 22:46:07 -0600

    Frame Cargar archivo

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from tkinter import *
 from tkinter import filedialog
 from clases import Token, Imagen
+import os
 
-texto_doc = ""
-texto_cargado = False
 tokens = []
 titulo = Token(1, "titulo")
 tokens.append(titulo)
@@ -46,6 +45,9 @@ tokens.append(separador)
 
 tokens_leidos = []
 imagenes_cargadas = []
+texto_doc = ""
+texto_cargado = False
+name_archivo_actual = ""
 
 class Interfaz:
     def __init__(self, ventana):
@@ -65,11 +67,17 @@ class Interfaz:
         self.frame3 = LabelFrame(self.window,bg="white", text="Imágenes")
         self.frame2 = LabelFrame(self.window,bg="white", text="Analizar Archivo")
         self.frame1 = LabelFrame(self.window,bg="white", text="Cagar Archivo")
+        self.frame_no_file = Frame(self.frame1, bg="white")
+        self.frame_no_file.place(x=0, y=0, relheight=1, relwidth=1)
+        load_img = PhotoImage(file="images/load.png")
+        load_lb = Label(self.frame_no_file, image=load_img, bg="white")
+        load_lb.photo = load_img
+        load_lb.place(x=10, y=10, width=300, height=300)
+        title1= Label(self.frame_no_file, text="No se ha cargado ningún archivo al programa.", font=("Consolas", 20), bg="white")
+        title1.place(x=320, y=120)
         
         for frame in (self.frame1, self.frame2, self.frame3, self.frame4):
             frame.place(x=300, y=280, width=1000, height=350)
-            title = Label(frame, text="TITULO DE PRUEBA")
-            title.pack()
 
         frame_btn = Frame(self.window, bg="cornsilk")
         frame_btn.place(x=300, y=200)
@@ -92,8 +100,8 @@ class Interfaz:
     def abrirArchivo(self):
         global texto_doc
         global texto_cargado
-        print('--> Se ha abierto la ventana para seleccionar el archivo')
-        archivo = filedialog.askopenfile(
+        global name_archivo_actual
+        name_file = filedialog.askopenfilename(
             title = "Seleccionar archivo PXLA",
             initialdir = "./",
             filetypes = {
@@ -102,13 +110,26 @@ class Interfaz:
             }
         )
         try:
+            archivo = open(name_file)
             texto = archivo.read()
             texto_doc = texto
             texto_cargado = True
-            print("Archivo leído con éxito")
+            self.frame_file = Frame(self.frame1, bg="white")
+            self.frame_file.place(x=0, y=0, relheight=1, relwidth=1)
+            load_img = PhotoImage(file="images/loaded.png")
+            load_lb = Label(self.frame_file, image=load_img, bg="white")
+            load_lb.photo = load_img
+            load_lb.place(x=10, y=10, width=300, height=300)
+            title1= Label(self.frame_file, text="El archivo se encuentra cargado al programa.", font=("Consolas", 20), bg="white")
+            title1.place(x=320, y=110)
+            name_archivo_actual = os.path.basename(name_file)
+            title2= Label(self.frame_file, text=name_archivo_actual, font=("Consolas", 20), bg="white")
+            title2.place(x=320, y=150)
+            print("->Archivo leído con éxito")
             archivo.close()
-        except:
-            print("No se seleccionó un archivo")
+        except Exception as e:
+            print(e)
+            print("->No se seleccionó un archivo")
 
 if __name__ == '__main__':
     ventana = Tk()
